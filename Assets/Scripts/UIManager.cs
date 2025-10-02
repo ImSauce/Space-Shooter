89ec8f2c794c09cc8e010fffac26a71e7b995523
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-10-02 23:49:34 +0800

    game over and restart

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,4 +1,5 @@
 //  [小鈎ハレ]  //
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,13 +11,21 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Image _LivesImg;
 
+    [SerializeField]
+    private Text _gameOverText;
 
     [SerializeField]
     private Text _scoreText;
 
+    [SerializeField]
+    private Text _restartText;
+
+    private GameManager _gameManager;
+
     void Start()
     {
         _scoreText.text = "Score: " + 0;
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     public void UpdateScore(int playerScore)
@@ -27,6 +36,30 @@ public class UIManager : MonoBehaviour
     public void UpdateLives(int currentLives)
     {
         _LivesImg.sprite = _liveSprites[currentLives];
+
+        if (currentLives == 0)
+        {
+            GameOverSequence();
+        }
+    }
+
+    void GameOverSequence()
+    {
+        _gameManager.GameOver();
+        _gameOverText.gameObject.SetActive(true);
+        _restartText.gameObject.SetActive(true);
+        StartCoroutine(GameOverFlickerRoutine());
+    }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while (true)
+        {
+            _gameOverText.text = "GAME OVER";
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.text = "";
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 
     
